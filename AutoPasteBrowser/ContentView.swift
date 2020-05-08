9327ffed01746d5bcd61deeab82425dbf8f9d358
wataruiijima <wataru.iijima@gmail.com>
2020-05-08 10:41:51 +0900

    Initial Commit

---
+++ AutoPasteBrowser/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AutoPasteBrowser
+//
+//  Created by w-iijima on 2020/05/08.
+//  Copyright © 2020 omuriceman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
